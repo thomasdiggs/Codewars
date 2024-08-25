28cabd8260976b75550e35a456ebed07e729f9c9
Thomas Diggs <thomasdiggs@gmail.com>
2024-08-24 20:46:18 -0600

    Completed challenge Convert_boolean_values_to_strings_'Yes'_or_'No'

---
+++ C#/Convert_boolean_values_to_strings_'Yes'_or_'No'/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Convert_boolean_values_to_strings__Yes__or__No_
+{
+    internal class Program
+    {
+        public static string BoolToWord(bool value)
+        {
+            return value ? "Yes" : "No";
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(BoolToWord(true)); // Yes
+            Console.WriteLine(BoolToWord(false)); // No
+        }
+    }
+}
