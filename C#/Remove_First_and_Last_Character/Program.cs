f49ad7e2de5128b987a6e7dfa5371bb228d4cae0
Thomas Diggs <thomasdiggs@gmail.com>
2024-08-29 20:00:28 -0600

    Challenge Remove_First_and_Last_Character complete

---
+++ C#/Remove_First_and_Last_Character/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Remove_First_and_Last_Character
+{
+    internal class Program
+    {
+        public static string Remove_char(string s)
+        {
+            //return s.Substring(1, s.Length - 2);
+            // Also could do this, don't forget this clever way!
+            return s[1..^1];
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Remove_char("eloquent")); // "loquen"
+            Console.WriteLine(Remove_char("country")); // "ountr"
+            Console.WriteLine(Remove_char("person")); // "erso"
+            Console.WriteLine(Remove_char("place")); // "lac"
+        }
+    }
+}
