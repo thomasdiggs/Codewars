bfa877532669c4f3eb9fdcaf85410f8f821ea9ae
Thomas Diggs <thomasdiggs@gmail.com>
2024-08-31 20:09:42 -0600

    Challenge String_repeat in C# complete

---
+++ C#/String_repeat/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace String_repeat
+{
+    internal class Program
+    {
+        public static string RepeatStr(int n, string s)
+        {
+            string output = "";
+            for (int i = 0; i < n; i++)
+            {
+                output += s;
+            }
+            return output;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(RepeatStr(6, "I"));   // "IIIIII"
+            Console.WriteLine(RepeatStr(5, "#"));   // "#####"
+            Console.WriteLine(RepeatStr(2, "ha"));  // "haha"
+        }
+    }
+}
