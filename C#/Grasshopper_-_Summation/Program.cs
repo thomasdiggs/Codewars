29ab942e4c2aac45a307d806a0f0195162938d65
Thomas Diggs <thomasdiggs@gmail.com>
2024-09-02 21:29:11 -0600

    Challenge Grasshopper_-_Summation in C# complete

---
+++ C#/Grasshopper_-_Summation/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Grasshopper___Summation
+{
+    internal class Program
+    {
+        public static int Summation(int num)
+        {
+            int sum = 0;
+            for (int i = 1; i <= num; i++)
+            {
+                sum += i;
+            }
+            return sum;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Summation(1));    // 1
+            Console.WriteLine(Summation(8));    // 36
+            Console.WriteLine(Summation(22));   // 253
+        }
+    }
+}
