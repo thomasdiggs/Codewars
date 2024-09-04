198167c07615c246dd555f5faebdb4fd8379643d
Thomas Diggs <thomasdiggs@gmail.com>
2024-09-03 21:53:53 -0600

    Challenge Find_the_smallest_integer_in_the_array in C# complete

---
+++ C#/Find_the_smallest_integer_in_the_array/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Find_the_smallest_integer_in_the_array
+{
+    internal class Program
+    {
+        public static int FindSmallestInt(int[] args)
+        {
+            int smallest = args[0];
+            for (int i = 1; i < args.Length; i++)
+            {
+                if (args[i] < smallest)
+                {
+                    smallest = args[i];
+                }
+            }
+            return smallest;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FindSmallestInt(new int[] { 34, 15, 88, 2 }));        // 2
+            Console.WriteLine(FindSmallestInt(new int[] { 34, -345, -1, 100 }));    // -345
+        }
+    }
+}
