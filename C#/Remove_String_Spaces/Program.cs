7c7661647e66ef96f64f8df1ab5aa6680db4531a
Thomas Diggs <thomasdiggs@gmail.com>
2024-09-04 20:17:50 -0600

    Challenge Remove_String_Spaces in C# complete

---
+++ C#/Remove_String_Spaces/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Remove_String_Spaces
+{
+    internal class Program
+    {
+        public static string NoSpace(string input)
+        {
+            return input.Replace(" ", "");
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(NoSpace("8 j 8   mBliB8g  imjB8B8  jl  B"));              // 8j8mBliB8gimjB8B8jlB
+            Console.WriteLine(NoSpace("8 8 Bi fk8h B 8 BB8B B B  B888 c hl8 BhB fd"));  // 88Bifk8hB8BB8BBB888chl8BhBfd
+        }
+    }
+}
