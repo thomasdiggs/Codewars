8dbb223c7e4819e741b5dc5ae36a84f96fb10323
Thomas Diggs <thomasdiggs@gmail.com>
2024-09-05 20:36:36 -0600

    Challenge Count_sheep in C# complete

---
+++ C#/Counting_sheep/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Counting_sheep
+{
+    internal class Program
+    {
+        public static int CountSheeps(bool[] sheeps)
+        {
+            int counter = 0;
+            foreach (bool sheep in sheeps)
+            {
+                if (sheep == true)
+                {
+                    counter++;
+                }
+            }
+            return counter;
+        }
+
+        static void Main(string[] args)
+        {
+            bool[] sheeps = { true, false, true, true, false, true, true, true, false, true, true, false, true, false, false, true, true, true, true, true, false, false, true, true };
+            Console.WriteLine(CountSheeps(sheeps));  // 16
+        }
+    }
+}
--- C#/Remove_String_Spaces/README.md
+++ C#/Remove_String_Spaces/README.md
@@ -3,6 +3,7 @@
 Write a function that removes the spaces from the string, then return the resultant string.
 
 ## Examples:
+
 ```
 Input -> Output
 "8 j 8   mBliB8g  imjB8B8  jl  B" -> "8j8mBliB8gimjB8B8jlB"
