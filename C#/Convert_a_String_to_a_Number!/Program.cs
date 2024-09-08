3d1d3daad4bac6a63c22f71320b759ebac94c2d8
Thomas Diggs <thomasdiggs@gmail.com>
2024-09-07 19:49:50 -0600

    Challenge Convert_a_String_to_a_Number! in C# complete

---
+++ C#/Convert_a_String_to_a_Number!/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Convert_a_String_to_a_Number_
+{
+    internal class Program
+    {
+        public static int StringToNumber(String str)
+        {
+            return Convert.ToInt32(str);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(StringToNumber("1234"));  // 1234
+            Console.WriteLine(StringToNumber("605"));   // 605
+        }
+    }
+}
