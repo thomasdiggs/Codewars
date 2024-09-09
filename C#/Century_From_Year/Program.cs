b63720890d7b284e41afd421a056fc3b63f162bd
Thomas Diggs <thomasdiggs@gmail.com>
2024-09-08 20:06:54 -0600

    Challenge Century_From_Year in C# complete

---
+++ C#/Century_From_Year/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Century_From_Year
+{
+    internal class Program
+    {
+        public static int СenturyFromYear(int year)
+        {
+            return year % 100 == 0 ? year / 100 : year / 100 + 1;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(СenturyFromYear(1705));    // 18
+            Console.WriteLine(СenturyFromYear(1900));    // 19
+            Console.WriteLine(СenturyFromYear(1601));    // 17
+            Console.WriteLine(СenturyFromYear(2000));    // 20
+            Console.WriteLine(СenturyFromYear(89));      // 1
+        }
+    }
+}
