79c29b2a8ec055d5cf8fbb6e56ff2c1c3e815023
Thomas Diggs <thomasdiggs@gmail.com>
2024-09-22 18:14:04 -0600

    Challenge Returning_Strings in C# complete

---
+++ C#/Returning_Strings/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Returning_Strings
+{
+    internal class Program
+    {
+        public static string Greet(string name)
+        {
+            return $"Hello, {name} how are you doing today?";
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Greet("Thomas"));   // Hello, Thomas how are you doing today?
+        }
+    }
+}
