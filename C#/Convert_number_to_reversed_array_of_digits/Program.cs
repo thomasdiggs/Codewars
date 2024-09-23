c69cd3692789e7348552603d2465dad9d8e0ca56
Thomas Diggs <thomasdiggs@gmail.com>
2024-09-22 18:09:10 -0600

    Challenge Convert_number_to_reversed_array_of_digits in C# complete

---
+++ C#/Convert_number_to_reversed_array_of_digits/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Convert_number_to_reversed_array_of_digits
+{
+    internal class Program
+    {
+        public static long[] Digitize(long n)
+        {
+            string s = n.ToString();
+            long[] arr = new long[s.Length];
+            for (int i = 0; i < s.Length; i++)
+            {
+                arr[i] = long.Parse(s[s.Length - i - 1].ToString());
+            }
+            return arr;
+        }
+
+        static void Main(string[] args)
+        {
+            long[] arr = Digitize(12345);   // [5, 4, 3, 2, 1]
+            foreach (var i in arr)
+            {
+                Console.Write(i + " ");
+            }
+        }
+    }
+}
