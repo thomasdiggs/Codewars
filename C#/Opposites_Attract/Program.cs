a3169247d6be3a2b963ce2abfa5d35fd6d46f21f
Thomas Diggs <thomasdiggs@gmail.com>
2024-09-23 21:26:36 -0600

    Challenge Opposites_Attract in C# complete

---
+++ C#/Opposites_Attract/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Opposites_Attract
+{
+    internal class Program
+    {
+        public static bool lovefunc(int flower1, int flower2)
+        {
+            return flower1 % 2 != flower2 % 2;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(lovefunc(1, 4)); // True
+            Console.WriteLine(lovefunc(2, 2)); // False
+            Console.WriteLine(lovefunc(0, 1)); // True
+            Console.WriteLine(lovefunc(0, 0)); // False
+        }
+    }
+}
