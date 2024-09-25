bc474cf6fbab429d4e60b72edc335e7743ec0cfa
Thomas Diggs <thomasdiggs@gmail.com>
2024-09-24 20:03:13 -0600

    Challenge A_Needle_in_the_Haystack and Function_1_-_hello_world in C# complete

---
+++ C#/A_Needle_in_the_Haystack/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace A_Needle_in_the_Haystack
+{
+    internal class Program
+    {
+        public static string FindNeedle(object[] haystack)
+        {
+            return $"found the needle at position {Array.IndexOf(haystack, "needle")}";
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FindNeedle(new object[] { '3', "123124234", null, "needle", "world", "hay", 2, '3', true, false })); // found the needle at position 3
+        }
+    }
+}
---
+++ C#/Function_1_-_hello_world/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Function_1___hello_world
+{
+    internal class Program
+    {
+        public static string greet() => "hello world!";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(greet());  // hello world!
+        }
+    }
+}
