ab439f1f8697cef7ae2816c86951121b03e1c671
Thomas Diggs <thomasdiggs@gmail.com>
2024-09-26 20:32:05 -0600

    Challenge Is_n_divisible_by_x_and_y in C# complete

---
+++ C#/Is_n_divisible_by_x_any_y/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Is_n_divisible_by_x_any_y
+{
+    internal class Program
+    {
+        public static bool IsDivisible(int n, int x, int y)
+        {
+            if (n % x == 0 && n % y == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        // Also could have done this (less readable in my opinion)
+        // public static bool IsDivisible(int n, int x, int y) => n % x == 0 && n % y == 0;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsDivisible(12, 4, 3));   // True
+            Console.WriteLine(IsDivisible(12, 5, 3));   // False
+        }
+    }
+}
