80508ae88f75dbede9d68fe79917fac37b619b8b
Thomas Diggs <thomasdiggs@gmail.com>
2024-09-30 19:42:57 -0600

    Restructure directories. Challenge Vowel_Count in C# complete

---
+++ C#/7_kyu/Vowel_Count/Program.cs
@@ -0,0 +1,28 @@
+﻿
+namespace Vowel_Count
+{
+    internal class Program
+    {
+        public static int GetVowelCunt(string str)
+        {
+            char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
+            int count = 0;
+            char[] strArray = str.ToCharArray();
+            foreach (var c in strArray)
+            {
+                if (vowels.Contains(c))
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetVowelCunt("hello"));           // 2
+            Console.WriteLine(GetVowelCunt("why"));             // 0
+            Console.WriteLine(GetVowelCunt("why do you ask?")); // 4
+        }
+    }
+}
