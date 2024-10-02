3806a0af28c99d6296b6cdd5b6d58e665215cd07
Thomas Diggs <thomasdiggs@gmail.com>
2024-10-01 20:46:19 -0600

    Changes to Beginner_-_Lost_Without_a_Map in C#

--- C#/8_kyu/Beginner_-_Lost_Without_a_Map/Program.cs
+++ C#/8_kyu/Beginner_-_Lost_Without_a_Map/Program.cs
@@ -25,34 +25,11 @@ namespace Beginner___Lost_Without_a_Map
             return y;
         }
 
-        public static int[] Maps2(int[] x)
-        {
-            for (int i = 0; i < x.Length; i++)
-            {
-                x[i] *= 2;
-            }
-            return x;
-        }
-
         static void Main(string[] args)
         {
             int[] x = { 1, 2, 3, 4, 5 };
             int[] y = Maps(x);
             foreach (int i in y)
-            {
-                Console.WriteLine(i);
-            }
-
-            foreach (int i in x)
-            {
-                Console.WriteLine(i);
-            }
-            int[] test = Maps2(x);
-            foreach (int i in test)
-            {
-                Console.WriteLine(i);
-            }
-            foreach (int i in x)
             {
                 Console.WriteLine(i);
             }
