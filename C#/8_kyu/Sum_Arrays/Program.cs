f8c5022be424d40fff2786b30e3c8aeff0dffcda
Thomas Diggs <thomasdiggs@gmail.com>
2024-10-03 19:01:45 -0600

    Challenge Sum_Arrays in C# complete

---
+++ C#/8_kyu/Sum_Arrays/Program.cs
@@ -0,0 +1,27 @@
+﻿
+namespace Sum_Arrays
+{
+    internal class Program
+    {
+        public static double SumArray(double[] array)
+        {
+            double sum = 0.0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                sum += array[i];
+            }
+            return sum;
+        }
+
+        static void Main(string[] args)
+        {
+            double[] array = { 1.0, 2.0, 3.0, 4.0, 5.0 };
+            double sum = SumArray(array);
+            Console.WriteLine("Sum of array elements: " + sum); // 15
+
+            // Of course we could do the following:
+            Console.WriteLine(array.Sum()); // 15
+            // But the function above shows the inner workings.
+        }
+    }
+}
