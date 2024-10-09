255fd1c3a5fb43450c7c8aa1c365aba58fe7c8fd
Thomas Diggs <thomasdiggs@gmail.com>
2024-10-08 20:12:08 -0600

    Challenge How_good_are_you_really in C# complete

---
+++ C#/8_kyu/How_good_are_you_really/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+
+namespace How_good_are_you_really
+{
+    internal class Program
+    {
+        public static bool BetterThanAverage(int[] ClassPoints, int YourPoints)
+        {
+            int sum = 0, avg = 0;
+            for (int i = 0; i < ClassPoints.Length; i++)
+            {
+                sum += ClassPoints[i];
+            }
+            avg = sum / ClassPoints.Length;
+            return YourPoints > avg;
+        }
+
+        public static bool BetterWayToBetterThanAverage(int[] ClassPoints, int YourPoints)
+        {
+            return YourPoints > ClassPoints.Average();
+        }
+
+        static void Main(string[] args)
+        {
+            int[] classPoints = { 100, 101, 102, 103 };
+            int yourPoints = 102;
+            Console.WriteLine(BetterThanAverage(classPoints, yourPoints));  // True
+            Console.WriteLine(BetterWayToBetterThanAverage(classPoints, yourPoints));  // True
+        }
+    }
+}
