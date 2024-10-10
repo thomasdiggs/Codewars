7c985456190262725550a19e1085aa432cc94690
Thomas Diggs <thomasdiggs@gmail.com>
2024-10-09 20:19:47 -0600

    Challenge Calculate_average in C# complete

---
+++ C#/8_kyu/Calculate_average/Program.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Calculate_average
+{
+    internal class Program
+    {
+        public static double FindAverage(double[] array) => array.Length == 0 ? 0 : array.Average();
+
+        static void Main(string[] args)
+        {
+            double[] array = { 1, 2, 3, 4, 5 };
+            Console.WriteLine(FindAverage(array));  // 3
+        }
+    }
+}
