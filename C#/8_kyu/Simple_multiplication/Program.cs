1d8360b4973fa216302efee5a9f1d6d4a290c49f
Thomas Diggs <thomasdiggs@gmail.com>
2024-10-10 19:37:24 -0600

    Challenge Simple_multiplication in C# complete

---
+++ C#/8_kyu/Simple_multiplication/Program.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Simple_multiplication
+{
+    internal class Program
+    {
+        public static int Multiply(int x)
+        {
+            return x % 2 == 0 ? x * 8 : x * 9;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Multiply(5));  // 45
+        }
+    }
+}
