c768d57595a2c7dc3d52bd1969dbaf3707025079
Thomas Diggs <thomasdiggs@gmail.com>
2024-10-11 20:27:42 -0600

    Challenge MakeUpperCase in C# complete

---
+++ C#/8_kyu/MakeUpperCase/Program.cs
@@ -0,0 +1,13 @@
+﻿
+namespace MakeUpperCase
+{
+    internal class Program
+    {
+        public static string MakeUpperCase(string str) => str.ToUpper();
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MakeUpperCase("hello"));  // HELLO
+        }
+    }
+}
