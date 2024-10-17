09078a7d0835e4aa2a37fab2ac41dda1d1166803
Thomas Diggs <thomasdiggs@gmail.com>
2024-10-16 19:13:05 -0600

    Challenge You_only_need_one_-_Beginner in C# complete

---
+++ C#/8_kyu/You_only_need_one_-_Beginner/Program.cs
@@ -0,0 +1,28 @@
+﻿
+namespace You_only_need_one___Beginner
+{
+    internal class Program
+    {
+        // Array can contain numbers and strings. X can be either.
+        public static bool Check(object[] a, object x)
+        {
+            bool found = false;
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (a[i].Equals(x))
+                {
+                    found = true;
+                }
+            }
+            return found;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Check(new object[] { 66, 101 }, 66)); // true
+            Console.WriteLine(Check(new object[] { 80, 117, 115, 4, 97, 118, 97 }, 107)); // false
+            Console.WriteLine(Check(new object[] { 't', 'e', 's', 't' }, 'e')); // true
+            Console.WriteLine(Check(new object[] { "what", "a", "great", "kata" }, "kat")); // false
+        }
+    }
+}
