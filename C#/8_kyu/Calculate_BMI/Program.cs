9a1122765338196ca06ebf1392be858a6ee0f1ed
Thomas Diggs <thomasdiggs@gmail.com>
2024-10-17 19:29:48 -0600

    Challenge Calculate_BMI in C# complete

---
+++ C#/8_kyu/Calculate_BMI/Program.cs
@@ -0,0 +1,13 @@
+﻿static string Bmi(double weight, double height)
+{
+    double bmi = weight / (height * height);
+    return bmi <= 18.5 ? "Underweight" :
+        bmi <= 25.0 ? "Normal" :
+        bmi <= 30.0 ? "Overweight" :
+        bmi > 30 ? "Obese": "";
+}
+
+Console.WriteLine(Bmi(50, 1.80)); // Underweight
+Console.WriteLine(Bmi(80, 1.80)); // Normal
+Console.WriteLine(Bmi(90, 1.80)); // Overweight
+Console.WriteLine(Bmi(110, 1.80)); // Obese
