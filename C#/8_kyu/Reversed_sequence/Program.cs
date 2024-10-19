bd4686e97d8730e00f3d26cb4518b184dfca8681
Thomas Diggs <thomasdiggs@gmail.com>
2024-10-19 10:22:54 -0600

    Challenge Reversed_sequence in C# complete

---
+++ C#/8_kyu/Reversed_sequence/Program.cs
@@ -0,0 +1,21 @@
+﻿static int[] ReverseSeq(int n)
+{
+    int[] result = new int[n];
+    for (int i = 0; i < n; i++)
+    {
+        result[i] = n - i;
+    }
+    return result;
+    
+    // This is also a good solution, but as with most of these
+    // challenges, using libraries like this abstracts away the
+    // underlying logic and doesn't help you learn as much.
+
+    // return Enumerable.Range(1, n).Reverse().ToArray();
+}
+
+int[] ints = ReverseSeq(5);
+foreach (int i in ints)
+{
+    Console.WriteLine(i);
+}
