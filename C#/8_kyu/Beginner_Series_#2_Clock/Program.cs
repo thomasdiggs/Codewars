3729a340bcc2eac936a63714076c8aeab09284d5
Thomas Diggs <thomasdiggs@gmail.com>
2024-10-19 08:41:51 -0600

    Challenge Beginner_Series_#2_Clock in C# complete

---
+++ C#/8_kyu/Beginner_Series_#2_Clock/Program.cs
@@ -0,0 +1,7 @@
+﻿int Past(int h, int m, int s)
+{
+    return h * 60 * 60 * 1000 + m * 60 * 1000 + s * 1000;
+}
+
+Console.WriteLine(Past(0, 1, 1)); // 61000
+Console.WriteLine(Past(1, 1, 1)); // 3661000
