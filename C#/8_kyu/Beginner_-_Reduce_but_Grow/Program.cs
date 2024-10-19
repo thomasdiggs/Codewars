0b6215f869c8c3db026d98e2e4723ed2a5bda143
Thomas Diggs <thomasdiggs@gmail.com>
2024-10-19 08:47:43 -0600

    Challenge Beginner_-_Reduce_but_Grow in C# complete

---
+++ C#/8_kyu/Beginner_-_Reduce_but_Grow/Program.cs
@@ -0,0 +1,11 @@
+﻿int Grow(int[] x)
+{
+    int result = 1;
+    for (int i = 0; i < x.Length; i++)
+    {
+        result *= x[i];
+    }
+    return result;
+}
+
+Console.WriteLine(Grow([1, 2, 3, 4])); // 24
