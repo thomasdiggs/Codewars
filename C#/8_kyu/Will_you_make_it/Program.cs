2f38158ff735106e23ddb821390aeec56167b9d8
Thomas Diggs <thomasdiggs@gmail.com>
2024-10-23 19:20:30 -0600

    Challenge Will_you_make_it in C# complete

---
+++ C#/8_kyu/Will_you_make_it/Program.cs
@@ -0,0 +1,4 @@
+﻿static bool ZeroFuel(uint distanceToPump, uint mpg, uint fuelLeft) => distanceToPump <= mpg * fuelLeft;
+
+Console.WriteLine(ZeroFuel(50, 25, 2)); // true
+Console.WriteLine(ZeroFuel(100, 50, 1)); // false
