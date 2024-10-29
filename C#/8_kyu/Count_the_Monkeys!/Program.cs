7e2462e986529e413944a941905f88da3e142b2e
Thomas Diggs <thomasdiggs@gmail.com>
2024-10-28 18:25:09 -0600

    Challenge Count_the_Monkeys! in C# complete

---
+++ C#/8_kyu/Count_the_Monkeys!/Program.cs
@@ -0,0 +1,18 @@
+﻿static int[] MonkeyCount(int n)
+{
+    int[] result = new int[n];
+    for (int i = 0; i < n; i++)
+    {
+        result[i] = i + 1;
+    }
+    return result;
+}
+
+// Also this!
+//static int[] MonkeyCount(int n) => Enumerable.Range(1, n).ToArray();
+
+int[] ints = MonkeyCount(5);
+foreach (int i in ints)
+{
+    Console.WriteLine(i);
+}
